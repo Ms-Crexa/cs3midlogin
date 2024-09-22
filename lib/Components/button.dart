ce48d4a3f0be94d1a39ad1badf81b0777105cb37
Ninya <mnamparaiso@addu.edu.ph>
2024-09-23 00:25:21 +0800

    missing: google icons and nav route

--- lib/Components/button.dart
+++ lib/Components/button.dart
@@ -4,19 +4,26 @@ Widget customelvatedButton({
   required String text,
   required Color backgroundColor,
   required Color textColor,
+  required double width,
   required VoidCallback onPressed,
 }) {
-  return ElevatedButton(
-    style: ElevatedButton.styleFrom(
-      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-      backgroundColor: backgroundColor,
-      foregroundColor: textColor,
-      elevation: 8,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
+  return SizedBox(
+    width: width,
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        backgroundColor: backgroundColor,
+        foregroundColor: textColor,
+        elevation: 8,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 20),
       ),
     ),
-    onPressed: onPressed,
-    child: Text(text),
   );
 }
--- lib/landingpage.dart
+++ lib/landingpage.dart
@@ -1,77 +1,159 @@
 import 'package:flutter/material.dart';
 import 'Components/button.dart';
+import 'Components/card.dart';
 
 class Landingpage extends StatefulWidget {
-  const Landingpage({Key? key}) : super(key: key);
+  const Landingpage({super.key});
 
   @override
   _LandingpageState createState() => _LandingpageState();
 }
 
-class _LandingpageState extends State<Landingpage> {
+class _LandingpageState extends State<Landingpage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _offsetSignUpAnimation;
+  late Animation<Offset> _offsetSignInAnimation;
+  bool isSignUpVisible = false;
+  bool isSignInVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    // Initialize animations
+    _offsetSignUpAnimation = Tween<Offset>(
+      begin: const Offset(0.0, 1.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    _offsetSignInAnimation = Tween<Offset>(
+      begin: const Offset(0.0, 1.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  void toggleSignUpCard() {
+    setState(() {
+      isSignUpVisible = !isSignUpVisible;
+      isSignInVisible = false;
+    });
+    isSignUpVisible ? _controller.forward() : _controller.reverse();
+  }
+
+  void toggleSignInCard() {
+    setState(() {
+      isSignInVisible = !isSignInVisible;
+      isSignUpVisible = false;
+    });
+    isSignInVisible ? _controller.forward() : _controller.reverse();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Get screen width
     final screenWidth = MediaQuery.of(context).size.width;
-    // Check if screen is mobile or larger
+    final screenheight = MediaQuery.of(context).size.height;
     final isMobile = screenWidth < 600;
 
     return Scaffold(
-      body: Container(
-        width: screenWidth,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-              'https://www.shutterstock.com/image-photo/colorful-sunset-viewed-pink-sea-600nw-2239509385.jpg',
+      body: Stack(
+        children: [
+          Container(
+            width: screenWidth,
+            height: screenheight,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/bgimage.jpg'),
+                fit: BoxFit.cover,
+              ),
             ),
-            fit: BoxFit.cover,
+            child: Container(
+              margin: EdgeInsets.only(
+                  left: isMobile ? 50 : 90,
+                  top: isMobile ? 100 : 60,
+                  right: isMobile ? 50 : 0),
+              child: Column(
+                crossAxisAlignment: isMobile
+                    ? CrossAxisAlignment.start
+                    : CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/AppLogo.png',
+                    height: isMobile ? 80 : 150,
+                    width: isMobile ? 80 : 150,
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: isMobile ? 220 : double.infinity,
+                    child: Text(
+                      'Enjoy the trip with me',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                        fontSize: isMobile ? 35 : 66,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: isMobile ? screenheight * 0.35 : 90),
+                  Center(
+                    child: Column(
+                      children: [
+                        customelvatedButton(
+                          text: 'Sign up',
+                          width: 340,
+                          backgroundColor:
+                              const Color.fromARGB(255, 252, 135, 32),
+                          textColor: const Color.fromARGB(255, 255, 255, 255),
+                          onPressed: toggleSignUpCard,
+                        ),
+                        const SizedBox(height: 25),
+                        customelvatedButton(
+                          text: 'Sign in',
+                          width: 340,
+                          backgroundColor:
+                              const Color.fromARGB(255, 248, 248, 248),
+                          textColor: const Color.fromARGB(255, 252, 135, 3),
+                          onPressed: toggleSignInCard,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-        child: Container(
-          margin:
-              EdgeInsets.only(left: isMobile ? 20 : 0, top: isMobile ? 50 : 0),
-          child: Column(
-            crossAxisAlignment:
-                isMobile ? CrossAxisAlignment.start : CrossAxisAlignment.center,
-            mainAxisAlignment:
-                !isMobile ? MainAxisAlignment.center : MainAxisAlignment.start,
-            children: [
-              Image.asset(
-                'assets/AppLogo.png',
-                height: 100,
-                width: 100,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Enjoy the trip with me',
-                style: TextStyle(
-                  color: const Color.fromARGB(255, 255, 255, 255),
-                  fontSize: isMobile ? 18 : 36,
-                ),
-              ),
-              SizedBox(
-                height: isMobile ? 60 : 40,
-              ),
-              customelvatedButton(
-                text: 'Signup',
-                backgroundColor: Colors.black,
-                textColor: const Color.fromARGB(137, 255, 255, 255),
-                onPressed: () {},
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              customelvatedButton(
-                text: 'SignIn',
-                backgroundColor: Colors.black,
-                textColor: const Color.fromARGB(234, 255, 255, 255),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
+          // Ternary rendering for cards
+          isSignUpVisible
+              ? card(
+                  isVisible: true,
+                  offsetAnimation: _offsetSignUpAnimation,
+                  content: signUpContent(),
+                )
+              : isSignInVisible
+                  ? card(
+                      isVisible: true,
+                      offsetAnimation: _offsetSignInAnimation,
+                      content: signInContent(),
+                    )
+                  : const SizedBox.shrink(), // Fallback if neither is visible
+        ],
       ),
     );
   }
